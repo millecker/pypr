deea38fb32056d3ed018d983fe345b49871c5e13
millecker <martin.illecker@gmail.com>
2014-05-27 19:24:30 +0200

    Comment savefig command

--- examples/sample_gmm.py
+++ examples/sample_gmm.py
@@ -10,10 +10,10 @@ ccov = [ array([[1,0.4],[0.4,1]]), diag((1,2)), diag((0.4,0.1)) ]
 
 X = gmm.sample_gaussian_mixture(centroids, ccov, mc, samples=1000)
 plot(X[:,0], X[:,1], '.')
-savefig('sample_gmm_plot1.png')
+# savefig('sample_gmm_plot1.png')
 
 for i in range(len(mc)):
     x1, x2 = gmm.gauss_ellipse_2d(centroids[i], ccov[i])
     plot(x1, x2, 'k', linewidth=2)
 xlabel('$x_1$'); ylabel('$x_2$')
-savefig('sample_gmm_plot2.png')
+# savefig('sample_gmm_plot2.png')
